f4a8b095f1259add3729a78166383a0fcb0e703d
Oleksandr Shaposhnyk <bromodragonfly11@gmail.com>
2022-09-08 22:27:41 +0300

    add Product and User entities

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,27 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dump($productList);
         return $this->render('main/default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    #[Route('/product-add', name: 'product-add')]
+    public function productAdd(): Response
+    {
+        $product = new Product();
+        $product->setTitle('Product '.rand(1, 100));
+        $product->setPrice('100');
+        $product->setQuantity('10');
+        $product->setDescription('Test test');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default" : false})
+     */
+    private $isDeleted;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isIsDeleted(): ?bool
+    {
+        return $this->isDeleted;
+    }
+
+    public function setIsDeleted(bool $isDeleted): self
+    {
+        $this->isDeleted = $isDeleted;
+
+        return $this;
+    }
+}
